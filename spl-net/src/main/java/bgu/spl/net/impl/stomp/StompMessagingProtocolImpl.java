82ff927f2cd4e68c30454cf62eb13e1dc5f6f7bf
TomerAmran <50573231+TomerAmran@users.noreply.github.com>
2020-01-06 15:58:54 +0200

    stomp protocol first steps

--- spl-net/src/main/java/bgu/spl/net/impl/stomp/StompMessagingProtocolImpl.java
+++ spl-net/src/main/java/bgu/spl/net/impl/stomp/StompMessagingProtocolImpl.java
@@ -2,11 +2,15 @@ package bgu.spl.net.impl.stomp;
 
 import bgu.spl.net.api.StompMessagingProtocol;
 import bgu.spl.net.srv.Connections;
+import bgu.spl.net.srv.DataBase;
+
+import java.util.HashMap;
 
 public class StompMessagingProtocolImpl implements StompMessagingProtocol {
     //fields
     private int connectionId;
     private Connections<String> connections;
+    private DataBase database;
     /**
      * Used to initiate the current client protocol with it's personal connection ID and the connections implementation
      **/
@@ -14,12 +18,70 @@ public class StompMessagingProtocolImpl implements StompMessagingProtocol {
     public void start(int connectionId, Connections<String> connections){
         this.connectionId = connectionId;
         this.connections = connections;
+        database = DataBase.getInstance();
     }
     @Override
     public String process(String message){
+        StompFrame frame = StompFrame.parse(message);
+        switch (frame.getCommand()){
+            case CONNECT:
+                CONNECT_received(frame.getHeaders(), frame.getBody());
+                break;
+            case CONNECTED:
+                CONNECTED_received(frame.getHeaders(), frame.getBody());
+                break;
+            case MESSAGE:
+                MESSAGE_received(frame.getHeaders(), frame.getBody());
+                break;
+            case RECEIPT:
+                RECEIPT_received(frame.getHeaders(), frame.getBody());
+                break;
+            case ERROR:
+                ERROR_received(frame.getHeaders(), frame.getBody());
+                break;
+            case SEND:
+                SEND_received(frame.getHeaders(), frame.getBody());
+                break;
+            case SUBSCRIBE:
+                SUBSCRIBE_received(frame.getHeaders(), frame.getBody());
+                break;
+            case UNSUBSCRIBE:
+                UNSUBSCRIBE_received(frame.getHeaders(), frame.getBody());
+                break;
+            case DISCONNECT:
+                DISCONNECT_received(frame.getHeaders(), frame.getBody());
+                break;
+        }
         return null;
     }
+    private void CONNECT_received(HashMap<String,String> headers, String body){
+        //deal with accept-version?
+        //deal with host?
+        String username = headers.get("login");
+        String passcode = headers.get("passcode");
+        //new user
+        if(!database.DoesUserExists(username))
+            sendCONNECTED(headers);
+        else{//user exist
+            if(!database.IsPasswordCorrect(username,passcode));
+            //error managing. bahhhh
+        }
 
+    }
+    private void CONNECTED_received(HashMap<String,String> headers, String body){}
+    private void MESSAGE_received(HashMap<String,String> headers, String body){}
+    private void RECEIPT_received(HashMap<String,String> headers, String body){}
+    private void ERROR_received(HashMap<String,String> headers, String body){}
+    private void SEND_received(HashMap<String,String> headers, String body){}
+    private void SUBSCRIBE_received(HashMap<String,String> headers, String body){}
+    private void UNSUBSCRIBE_received(HashMap<String,String> headers, String body){}
+    private void DISCONNECT_received(HashMap<String,String> headers, String body){}
+    private void sendCONNECTED(HashMap<String,String> headers){
+        StompFrame frame = new StompFrame();
+        frame.setCommand("CONNECTED");
+        frame.addHeader("version",headers.get("accept-version"));
+        connections.send(connectionId, frame.toString());
+    }
     /**
      * @return true if the connection should be terminated
      */
