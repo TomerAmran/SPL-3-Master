21c007e919c1bf2d9fa2e4b7d092f82980abeb28
amrant@wincs.cs.bgu.ac.il <tomeramran>
2020-01-07 18:07:54 +0200

    start real server

--- spl-net/src/main/java/bgu/spl/net/api/StompMessagingProtocol.java
+++ spl-net/src/main/java/bgu/spl/net/api/StompMessagingProtocol.java
@@ -2,14 +2,13 @@ package bgu.spl.net.api;
 
 import bgu.spl.net.srv.Connections;
 
-public interface StompMessagingProtocol extends MessagingProtocol<String> {
-
+public interface StompMessagingProtocol  {
 	/**
 	 * Used to initiate the current client protocol with it's personal connection ID and the connections implementation
 	**/
     void start(int connectionId, Connections<String> connections);
     
-    String process(String message);
+    void process(String message);
 	
 	/**
      * @return true if the connection should be terminated
